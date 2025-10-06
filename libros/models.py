33907a24feda35eca45ff65e15694fbccd0d8469
DanielJimenez357 <daniel05jimenez357@gmail.com>
2025-10-06 13:53:08 +0200

    Conexion a base de datos establecida Modelo de libro realizado Metodos lista y detalles creados

--- libros/models.py
+++ libros/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Libro(models.Model):
+
+    isbn = models.CharField(max_length=20, primary_key=True)
+    titulo = models.CharField(max_length=50)
+    autor = models.CharField(max_length=50)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.name
--- libros/views.py
+++ libros/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Libro
 
 # Create your views here.
+
+def Lista(request):
+    lista = Libro.objects.all()
+
+    //devolvemos una coleccion en caso de añadir mas contenido en un futuro
+    context= {
+        'lista': lista,
+    }
+
+    //renderiza la plantilla que indicamos con la informacion que le pasamos
+    return render(request, 'lista.html', context)
+
+def Detalles(request, id):
+
+    //recogemos el libor identificandolo por la pk que recogemos de la plantilla
+    libro = get_object_or_404(Libro, pk=id)
+
+    context= {
+        'isbn': libro.isbn,
+        'titulo': libro.titulo,
+        'autor': libro.autor,
+        'descripcion': libro.descripcion,
+    }
+
+    return render(request, 'detalles.html', context)
